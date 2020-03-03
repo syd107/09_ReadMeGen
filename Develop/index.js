26d79119d10e1e63029a7b0cc9831ee1309e8447
Sydney Krantz <syd107@gmail.com>
2020-03-02 20:56:21 -0800

    initial commit

---
+++ Develop/index.js
@@ -0,0 +1,20 @@
+// fs
+// util
+// axios for git
+// inquirer
+
+const fs = require("fs");
+const api = require("./utils/api");
+const markdown = require("./utils/generateMarkdown");
+const axios = require("axios");
+const inquirer = require("inquirer");
+
+console.log("markdown working");
+
+const questions = [];
+
+function writeToFile(fileName, data) {}
+
+function init() {}
+
+init();
